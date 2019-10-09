e123ceaf4b3247e4d7b14e2f34aa6c99e39bd988
Somiacao <18210240048@fudan.edu.cn>
2019-10-09 13:36:54 +0800

    'jdbc'

--- src/main/java/com/meiyingying/springboot/service/ArticleRestService.java
+++ src/main/java/com/meiyingying/springboot/service/ArticleRestService.java
@@ -1,17 +1,4 @@
 package com.meiyingying.springboot.service;
 
-import com.meiyingying.springboot.model.AjaxResponse;
-import com.meiyingying.springboot.model.Article;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-
-@Slf4j
-@Service
 public class ArticleRestService {
-
-    public String saveArtice(Article article){
-        log.info("saveArticle:{}",article);
-        return "测试";
-    }
-
 }
